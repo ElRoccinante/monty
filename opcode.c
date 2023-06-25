704bd882d5410cfa3ad4bd97da741fc645cb1335
Alae Chouiyakh <alaechouiye@gmail.com>
2023-06-25 08:59:19 -0700

    ll

--- opcode.c
+++ opcode.c
@@ -1,26 +1,27 @@
 #include "monty.h"
 
 /**
- * select_op - Selects and returns the corresponding function pointer
- *             based on the given opcode.
- * @s: Pointer to the opcode string
- *
- * Return: Pointer to the function corresponding to the opcode,
- *         or NULL if no match is found.
- */
+  * select_op - Selects and returns the corresponding function pointer
+  *		based on the given opcode.
+  * @s: Pointer to the op string
+  *
+  * Return: Pointer to the function corresponding to the opcode,
+  *		or NULL if no match is found.
+  */
+
 void (*select_op(char *s))(stack_t **, unsigned int)
 {
-    int i;
-    instruction_t ops[] = {
-        { "push", push },
-        { "pall", pall }
-    };
+	int i;
+	instruction_t ops[] = {
+		{ "push", push },
+		{ "pall", pall }
+	};
 
-    for (i = 0; ops[i].opcode; i++)
-    {
-        if (strcmp(s, ops[i].opcode) == 0)
-            return (ops[i].f);
-    }
+	for (i = 0; ops[i].opcode; i++)
+	{
+		if (strcmp(s, ops[i].opcode) == 0)
+			return (ops[i].f);
+	}
 
-    return (NULL);
+	return (NULL);
 }
